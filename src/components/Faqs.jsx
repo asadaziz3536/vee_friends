0815dee71e33a73c1fb11a88e92f11ff0236c702
ranaahtisham24 <76520043+ranaahtisham24@users.noreply.github.com>
2021-11-12 15:42:35 +0500

    faq completed

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -6,11 +6,11 @@ const Faqs = () => {
         <div className='faq-main-wrapper'>
         <Container className='faq-container'>
             
-            <Accordion className='accordion-wrapper' style={{maxWidth: '1366px', margin: 'auto'}}>
-            <h2>Frequently Asked Questions</h2>
+            <Accordion className='accordion-wrapper' style={{maxWidth: '1366px', margin: 'auto', }}>
+            <h2 className='faq-header-font'>Frequently Asked Questions</h2>
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>What is VeeFriends?</Accordion.Header>
-                    <Accordion.Body>
+                    <Accordion.Header className='faq-same-heading'>What is VeeFriends?</Accordion.Header>
+                    <Accordion.Body className='faq-same-body'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -21,8 +21,8 @@ const Faqs = () => {
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                    <Accordion.Header>Why NFT's?</Accordion.Header>
-                    <Accordion.Body>
+                    <Accordion.Header className='faq-same-heading'>Why NFT's?</Accordion.Header>
+                    <Accordion.Body className='faq-same-body'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -35,8 +35,8 @@ const Faqs = () => {
 
 
                 <Accordion.Item eventKey="2">
-                    <Accordion.Header>What is VeeCon?</Accordion.Header>
-                    <Accordion.Body>
+                    <Accordion.Header className='faq-same-heading'>What is VeeCon?</Accordion.Header>
+                    <Accordion.Body className='faq-same-body'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -48,8 +48,8 @@ const Faqs = () => {
                     </Accordion.Item>
 
                     <Accordion.Item eventKey="3">
-                    <Accordion.Header>What is so special about this NFT collection?</Accordion.Header>
-                    <Accordion.Body>
+                    <Accordion.Header className='faq-same-heading'>What is so special about this NFT collection?</Accordion.Header>
+                    <Accordion.Body className='faq-same-body'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -61,8 +61,8 @@ const Faqs = () => {
                     </Accordion.Item>
 
                     <Accordion.Item eventKey="4">
-                    <Accordion.Header>Where can I interact with other VeeFriends NFT collectors?</Accordion.Header>
-                    <Accordion.Body>
+                    <Accordion.Header className='faq-same-heading'>Where can I interact with other VeeFriends NFT collectors?</Accordion.Header>
+                    <Accordion.Body className='faq-same-body'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
